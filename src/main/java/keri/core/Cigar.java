bdbcc0af204dd2750d16126696f4d4e0031a0903
Sotatek-HieuDo4 <hieu.do4@sotatek.com>
2024-10-24 10:33:04 +0700

    chore: upgrade lombok version

---
+++ src/main/java/keri/core/Cigar.java
@@ -0,0 +1,24 @@
+package keri.core;
+
+import keri.core.args.MatterArgs;
+
+public class Cigar extends Matter {
+    private Verfer _verfer;
+
+    public Cigar(MatterArgs args) {
+        this(args, null);
+    }
+
+    public Cigar(MatterArgs args, Verfer verfer) {
+        super(args);
+        this._verfer = verfer;
+    }
+
+    public Verfer getVerfer() {
+        return _verfer;
+    }
+
+    public void setVerfer(Verfer verfer) {
+        this._verfer = verfer;
+    }
+}
