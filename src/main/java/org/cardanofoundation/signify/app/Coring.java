b48d64a29fea48352f2ae9032ccd0c4f059fa9c8
trhius <149554550+Sotatek-HieuDo4@users.noreply.github.com>
2024-12-26 17:08:56 +0700

    feat: implement Challenges, KeyEvents and Config

--- src/main/java/org/cardanofoundation/signify/app/Coring.java
+++ src/main/java/org/cardanofoundation/signify/app/Coring.java
@@ -7,6 +7,7 @@ import org.cardanofoundation.signify.cesr.Codex;
 import org.cardanofoundation.signify.cesr.LazySodiumInstance;
 import org.cardanofoundation.signify.cesr.Salter;
 import org.cardanofoundation.signify.cesr.args.RawArgs;
+import org.cardanofoundation.signify.cesr.util.Utils;
 
 import java.net.http.HttpClient;
 
@@ -41,6 +42,36 @@ public class Coring {
         public KeyEvents(SignifyClient client) {
             this.client = client;
         }
-        // others functions
+
+        /**
+         * Retrieve the key state for an identifier
+         * @param pre Identifier prefix
+         * @return A map representing the key states
+         * @throws Exception if the fetch operation fails
+         */
+        public Object get(String pre) throws Exception {
+            String path = "/events?pre=" + pre;
+            String method = "GET";
+            return Utils.fromJson(client.fetch(path, method, null, null).body(), Object.class);
+        }
+    }
+
+    @Getter
+    public static class Config {
+        public final SignifyClient client;
+
+        /**
+         * KeyEvents
+         * @param client {SignifyClient}
+         */
+        public Config(SignifyClient client) {
+            this.client = client;
+        }
+
+        public Object get() throws Exception {
+            String path = "/config";
+            String method = "GET";
+            return Utils.fromJson(client.fetch(path, method, null, null).body(), Object.class);
+        }
     }
 }
