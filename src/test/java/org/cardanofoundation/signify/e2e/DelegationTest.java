ebc557b52ae2db18013f66d12e1f854f9607038d
Sotatek-HuyTran11 <huy.tran11@sotatek.com>
2025-01-09 11:26:58 +0700

    fix: Update Delegation Test

--- src/test/java/org/cardanofoundation/signify/e2e/DelegationTest.java
+++ src/test/java/org/cardanofoundation/signify/e2e/DelegationTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DelegationTest extends TestUtils {
     private final String url = "http://127.0.0.1:3901";
@@ -27,7 +28,7 @@ public class DelegationTest extends TestUtils {
     private TestSteps testSteps = new TestSteps();
     private Retry retry = new Retry();
 
-    @Disabled
+
     @Test
     void delegationTest() throws Exception {
         String bran1 = Coring.randomPasscode();
@@ -146,10 +147,12 @@ public class DelegationTest extends TestUtils {
         Map<String, Object> oobiBody = (Map<String, Object>) oobis;
         ArrayList<String> oobisResponse = (ArrayList<String>) oobiBody.get("oobis");
 
+        String oobi = oobisResponse.getFirst().split("/agent/")[0];
+        assertNotNull(oobi);
         String contactId = getOrCreateContact(
                 client1,
                 "delegate",
-                oobisResponse.getFirst().split("/agent/")[0]
+                oobi
         );
         assertEquals(aid2.getPrefix(), contactId);
     }
