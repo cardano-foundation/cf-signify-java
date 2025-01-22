d3f7314ed455b09f414dfbf1d6ec3d34a85e3200
Sotatek-HuyTran11 <huy.tran11@sotatek.com>
2025-01-22 23:59:27 +0700

    fix: Update code CredentialData

--- src/main/java/org/cardanofoundation/signify/app/credentialing/credentials/CredentialData.java
+++ src/main/java/org/cardanofoundation/signify/app/credentialing/credentials/CredentialData.java
@@ -1,8 +1,8 @@
 package org.cardanofoundation.signify.app.credentialing.credentials;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,20 +10,35 @@ import java.util.Map;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CredentialData {
+    @JsonProperty("v")
     private String v;
+    @JsonProperty("d")
     private String d;
+    @JsonProperty("u")
     private String u; // Privacy salt
+    @JsonProperty("i")
     private String i; // Issuer of the credential
+    @JsonProperty("ri")
     private String ri; // Registry id
+    @JsonProperty("s")
     private String s; // Schema id
+    @JsonProperty("a")
     private CredentialSubject a; // Credential subject data
+    @JsonProperty("e")
     private Map<String, Object> e; // Credential source section
+    @JsonProperty("r")
     private Map<String, Object> r; // Credential rules section
 
     @Getter
     @Setter
     @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class CredentialSubject {
         private String i; // Issuee, or holder of the credential.
         private String dt; // Timestamp of issuance.
