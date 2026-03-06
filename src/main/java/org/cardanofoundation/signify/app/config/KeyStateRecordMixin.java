28d2b6c2c83fce19ee5fcc05914b4527ed0cfb0c
Patrick Vu <patrick.vu@sotatek.com>
2026-03-06 15:42:49 +0700

    add UnweightedThreshold and WeightedThreshold for kt, nt of key state

--- src/main/java/org/cardanofoundation/signify/app/config/KeyStateRecordMixin.java
+++ src/main/java/org/cardanofoundation/signify/app/config/KeyStateRecordMixin.java
@@ -2,21 +2,18 @@ package org.cardanofoundation.signify.app.config;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 /**
- * Jackson mixin to override deserialization of kt and nt fields in KeyStateRecord.
- * Marks these fields to be ignored during normal deserialization,
- * then provides custom getter/setter with Object type.
+ * Jackson mixin to handle polymorphic deserialization of kt and nt fields in KeyStateRecord.
+ * These fields can be either String or List<String> in JSON.
+ * By declaring them as Object, Jackson deserializes them naturally without casting issues.
  */
 @JsonIgnoreProperties(value = {"kt", "nt"}, allowGetters = true, allowSetters = false)
 public abstract class KeyStateRecordMixin {
     
     @JsonProperty("kt")
-    @JsonDeserialize(using = KeyStateRecordKtDeserializer.class)
     private Object kt;
     
     @JsonProperty("nt")
-    @JsonDeserialize(using = KeyStateRecordKtDeserializer.class)
     private Object nt;
 }
