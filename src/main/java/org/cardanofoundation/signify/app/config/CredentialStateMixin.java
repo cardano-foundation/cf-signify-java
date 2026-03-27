16d20117b16997467a912722b3e5fd5c93556b77
Patrick Vu <patrick.vu@sotatek.com>
2026-03-28 00:14:24 +0700

    improve CredentialState (#90)
    * improve CredentialState
    
    * chore: re-generate with correct ancatc
    
    * chore: remove unnecessary credential deserializers
    
    ---------
    
    Co-authored-by: iFergal <fergaloconnor1998@gmail.com>

--- src/main/java/org/cardanofoundation/signify/app/config/CredentialStateMixin.java
+++ src/main/java/org/cardanofoundation/signify/app/config/CredentialStateMixin.java
@@ -1,7 +1,9 @@
 package org.cardanofoundation.signify.app.config;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonDeserialize(using = CredentialStateDeserializer.class)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class CredentialStateMixin {
 }
