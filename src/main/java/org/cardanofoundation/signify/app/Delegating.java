a690c7e34e19e47019046f467fdd9912aa96abc7
Patrick Vu <patrick.vu@sotatek.com>
2026-04-14 14:48:29 +0700

    improve EventResult

--- src/main/java/org/cardanofoundation/signify/app/Delegating.java
+++ src/main/java/org/cardanofoundation/signify/app/Delegating.java
@@ -5,7 +5,8 @@ import org.cardanofoundation.signify.app.clienting.SignifyClient;
 import org.cardanofoundation.signify.app.aiding.EventResult;
 import org.cardanofoundation.signify.app.aiding.InteractionResponse;
 import org.cardanofoundation.signify.cesr.exceptions.LibsodiumException;
-import org.cardanofoundation.signify.generated.keria.model.DelegationOperation;
+import org.cardanofoundation.signify.cesr.util.Utils;
+import org.cardanofoundation.signify.generated.keria.model.DelegatorOperation;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
@@ -31,7 +32,7 @@ public class Delegating {
          * @return The delegated approval result
          * @throws Exception if the fetch operation fails
          */
-        public EventResult<DelegationOperation> approve(String name, Object data) throws LibsodiumException, DigestException, IOException, InterruptedException {
+        public EventResult<DelegatorOperation> approve(String name, Object data) throws LibsodiumException, DigestException, IOException, InterruptedException {
             InteractionResponse interactionResponse = this.client
                 .identifiers()
                 .createInteract(name, data);
@@ -41,10 +42,11 @@ public class Delegating {
                 "POST",
                 interactionResponse.jsondata()
             );
-            return new EventResult<>(interactionResponse.serder(), interactionResponse.sigs(), res, DelegationOperation.class);
+            DelegatorOperation op = Utils.fromJson(res.body(), DelegatorOperation.class);
+            return new EventResult<>(interactionResponse.serder(), interactionResponse.sigs(), op);
         }
 
-        public EventResult<DelegationOperation> approve(String name) throws LibsodiumException, DigestException, IOException, InterruptedException {
+        public EventResult<DelegatorOperation> approve(String name) throws LibsodiumException, DigestException, IOException, InterruptedException {
             return this.approve(name, null);
         }
     }
--- src/main/java/org/cardanofoundation/signify/app/aiding/EventResult.java
+++ src/main/java/org/cardanofoundation/signify/app/aiding/EventResult.java
@@ -1,16 +1,13 @@
 package org.cardanofoundation.signify.app.aiding;
 
-import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.util.List;
 
 import org.cardanofoundation.signify.cesr.Serder;
-import org.cardanofoundation.signify.cesr.util.Utils;
 import org.cardanofoundation.signify.generated.keria.model.Operation;
 
-public record EventResult<T extends Operation>(Serder serder, List<String> sigs, HttpResponse<String> response, Class<T> opType) {
-
-    public T op() throws IOException {
-        return Utils.fromJson(response.body(), opType);
+public record EventResult<T extends Operation>(Serder serder, List<String> sigs, T opInstance) {
+    public T op() {
+        return opInstance;
     }
 }
